4afde395612669fe337f62132bc659aab63af765
Tramonique <tramoniquewellington@gmail.com>
2026-02-25 22:00:39 -0500

    Completed Profile Page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
 
 ###
@@ -15,7 +16,27 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Tramonique Wellington")
+
+def format_date_joined(date_joined):
+    return date_joined.strftime("%B, %Y")
+
+
+@app.route('/profile')
+def profile():
+    joined_date = datetime.date(2022, 9, 7)
+
+    return render_template(
+        'profile.html',
+        fullname='Tramonique Wellington',
+        username='tramonique',
+        location='Spanish Town, Jamaica',
+        joined=format_date_joined(joined_date),
+        bio='Computer Science student who procrastinates by learning random things.',
+        posts=53,
+        following=723,
+        followers=4280
+    )
 
 
 ###
